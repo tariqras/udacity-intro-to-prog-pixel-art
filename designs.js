2cb3c86a172cf5bce0802970ea16d14dc093a957
tariqras <dev@bluecrowtech.com>
2021-06-26 19:02:45 -0400

    added basic grid generation functionality

--- designs.js
+++ designs.js
@@ -2,9 +2,26 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+//Event listener for submit button
+
+
 
 function makeGrid() {
 
-// Your code goes here!
+	let canvas = document.querySelector('#pixelCanvas');
 
+	const n = 4;
+	const m = 4;
+
+	for (var i = 0; i < n; i = i + 1){
+		var row = canvas.insertRow();
+		for (var j = 0; j < m; j = j + 1){
+			var cell = row.insertCell();
+		}
+	}
 }
+
+
+makeGrid();
+
+console.log(document.querySelector('#pixelCanvas'));
