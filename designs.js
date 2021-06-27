487b7b7ffb74560ed9597d7cecd896ee4949ce10
tariqras <dev@bluecrowtech.com>
2021-06-26 21:03:03 -0400

    feature/make-grid  properly creating grid from user values but previous table is not clearing

--- designs.js
+++ designs.js
@@ -1,27 +1,29 @@
 // Select color input
+const colorPicker = document.querySelector('#colorPicker');
+
 // Select size input
+const sizePicker = document.getElementById('sizePicker');
+
+const canvas = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 //Event listener for submit button
+sizePicker.addEventListener('submit', function makeGrid(e) {
 
+	const canvasHeight = Number(document.getElementById('inputHeight').value);
+	const canvasWidth = Number(document.getElementById('inputWidth').value);
 
-
-function makeGrid() {
-
-	let canvas = document.querySelector('#pixelCanvas');
-
-	const n = 4;
-	const m = 4;
-
-	for (var i = 0; i < n; i = i + 1){
+	for (var i = 0; i < canvasHeight; i = i + 1){
 		var row = canvas.insertRow();
-		for (var j = 0; j < m; j = j + 1){
+		for (var j = 0; j < canvasWidth; j = j + 1){
 			var cell = row.insertCell();
 		}
 	}
-}
+
+	e.preventDefault();
+
+});
 
 
-makeGrid();
 
-console.log(document.querySelector('#pixelCanvas'));
+console.log(canvas);
