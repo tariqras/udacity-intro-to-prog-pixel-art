e4419029663ae7314002dec4d606e84a939bbf17
tariqras <dev@bluecrowtech.com>
2021-06-26 21:18:37 -0400

    feature/make-grid    trashid   canvas is properly being generated from user input using html table.  previous canvas is being cleared properly instead of being appended

--- designs.js
+++ designs.js
@@ -1,18 +1,27 @@
 // Select color input
 const colorPicker = document.querySelector('#colorPicker');
 
+
+
 // Select size input
 const sizePicker = document.getElementById('sizePicker');
 
 const canvas = document.getElementById('pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+
 //Event listener for submit button
 sizePicker.addEventListener('submit', function makeGrid(e) {
 
+	//Get the user input values
 	const canvasHeight = Number(document.getElementById('inputHeight').value);
 	const canvasWidth = Number(document.getElementById('inputWidth').value);
 
+	//Clear the previous canvas
+	while(canvas.rows.length >0){
+		canvas.deleteRow(0);
+	}
+
+	//Make the drawing canvas
 	for (var i = 0; i < canvasHeight; i = i + 1){
 		var row = canvas.insertRow();
 		for (var j = 0; j < canvasWidth; j = j + 1){
