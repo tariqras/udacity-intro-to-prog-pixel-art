bd6275a34a4af7f6c8429d1b7b95fcdd526fbb64
tariqras <dev@bluecrowtech.com>
2021-06-26 23:19:28 -0400

    feature/draw-on-canvas functions properly.  To Do code cleanup and style guide compliance

--- designs.js
+++ designs.js
@@ -1,62 +1,64 @@
 //tariqras  udacity-intro-to-programming nanodegree  pixel art maker  2021JUNE26
 
-
 // Select color input
 const colorPicker = document.querySelector('#colorPicker');
 
 // Select size input
 const sizePicker = document.getElementById('sizePicker');
 
+//Select canvas
 const canvas = document.getElementById('pixelCanvas');
 
 //Default color is black
 var myColor = '#000000';
 
-
 //Event listener for color picker
 colorPicker.addEventListener('input', function changeColor(e) {
-	myColor = colorPicker.value;
+    "use strict";
+    myColor = colorPicker.value;
 } );
 
 //Event listener for submit button
 sizePicker.addEventListener('submit', function makeGrid(e) {
 
-	//Get the user input values
-	const canvasHeight = Number(document.getElementById('inputHeight').value);
-	const canvasWidth = Number(document.getElementById('inputWidth').value);
+    //Get the user input values
+    const canvasHeight = Number(document.getElementById('inputHeight').value);
+    const canvasWidth = Number(document.getElementById('inputWidth').value);
 
-	//Clear the previous canvas
-	while(canvas.rows.length >0){
-		canvas.deleteRow(0);
-	}
+    //Clear the previous canvas
+    while(canvas.rows.length >0){
+        canvas.deleteRow(0);
+    }
 
-	//Make the drawing canvas
-	for (var i = 0; i < canvasHeight; i = i + 1){
-		var row = canvas.insertRow();
-		for (var j = 0; j < canvasWidth; j = j + 1){
-			var cell = row.insertCell();
-		}
-	}
+    //Make the drawing canvas
+    for (var i = 0; i < canvasHeight; i = i + 1){
+        var row = canvas.insertRow(i);
+        for (var j = 0; j < canvasWidth; j = j + 1){
+            const cell = row.insertCell(j);
+            cell.addEventListener("click", function(e){
+                cell.style.backgroundColor = myColor;
+                e.preventDefault();
+            });
+        }
+    }
 
-	e.preventDefault();
-
+    e.preventDefault();
 });
+
 
 //Check for user values within limits
 sizePicker.addEventListener('input', function (e){
-	const canvasHeight = document.getElementById('inputHeight');
-	const canvasWidth = document.getElementById('inputWidth');
-	if (Number(canvasHeight.value) > 30){
-		canvasHeight.value = '30';
-	}
+    const canvasHeight = document.getElementById('inputHeight');
+    const canvasWidth = document.getElementById('inputWidth');
+    if (Number(canvasHeight.value) > 12){
+        canvasHeight.value = '12';
+    }
 
-	if (Number(canvasWidth.value) > 50){
-		canvasWidth.value = '50';
-	}
+    if (Number(canvasWidth.value) > 18){
+        canvasWidth.value = '18';
+    }
 
-	e.preventDefault();
+    e.preventDefault();
 });
 
 
-
-console.log(canvas);
